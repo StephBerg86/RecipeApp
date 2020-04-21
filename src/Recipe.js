6758c768ed8a4aa8d75d4e170becebad3cad2106
StephBerg86 <info@stephaniebergman.nl>
2020-04-21 15:21:30 +0200

    added styling

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,18 @@
 import React from "react";
+import style from "./recipe.module.css";
 
 export default function Recipe({ title, calories, image, ingredients }) {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
       <ul>
+        <h4>Ingredients:</h4>
         {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
         ))}
       </ul>
-      <p>{calories}</p>
-      <img src={image} alt=""></img>
+      <h4>Calories: {Math.round(calories)}</h4>
+      <img className={style.image} src={image} alt=""></img>
     </div>
   );
 }
